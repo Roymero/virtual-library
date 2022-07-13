a4edc943a356ff38eabe502cdc228fdbf36cba73
Roy Romero <Romerorr94@gmail.com>
2022-07-13 18:02:02 -0400

    finished front end design started working on script

--- script.js
+++ script.js
@@ -1,2 +1,50 @@
 
 
+var currentTitle;
+var currentAuthor;
+var currentPages;
+var hasRead;
+
+var books = []
+
+
+function book(title, author, pages, read){
+
+    this.title = title;
+    this.author= author;
+    this.pages = pages;
+    this.read = read;
+  
+}
+
+function openModal(){
+
+
+
+}
+
+const addBookButton = document.getElementById('addBookButton');
+const bookModal = document.getElementById('bookModal');
+const submitButton = document.getElementById('submitButton');
+const overlay = document.getElementById('overlay');
+
+
+
+
+addBookButton.addEventListener("click", ()=>{
+
+    bookModal.classList.add('active');
+    overlay.classList.add('active');
+
+
+});
+
+
+
+
+submitButton.addEventListener("click", ()=>{
+
+    
+
+
+});
