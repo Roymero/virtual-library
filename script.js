ff313bbeca19466e825cabd8ef25c4959cf8a0dd
Roy Romero <Romerorr94@gmail.com>
2022-07-14 16:39:17 -0400

    added functions for + add book button

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 
+// Initial variables
 
 var currentTitle;
 var currentAuthor;
 var currentPages;
 var hasRead;
 
-var books = []
+// library to keep all books in
+var library = [];
+
+var libraryLength;
 
 
 function book(title, author, pages, read){
@@ -23,13 +27,41 @@ function openModal(){
 
 }
 
+
+
 const addBookButton = document.getElementById('addBookButton');
 const bookModal = document.getElementById('bookModal');
 const submitButton = document.getElementById('submitButton');
 const overlay = document.getElementById('overlay');
 
+const bookForm = document.getElementById('bookForm');
+const bookGrid = document.getElementById('bookGrid');
 
 
+
+function addNewBook(){
+
+    console.log("works");
+
+    const newCard = document.createElement("div");
+    const innerCardTitle = document.createElement('p');
+    const innerCardAuthor = document.createElement('p');
+    const innerCardPages = document.createElement('p');
+
+    bookGrid.appendChild(newCard);
+    newCard.classList.add('book-card')
+
+    innerCardTitle.textContent = '"Book of Houdini"';
+    innerCardAuthor.textContent = 'Roy';
+    innerCardPages.textContent = '420';
+
+
+}
+
+function removeModal(){
+    overlay.classList.remove('active');
+    bookModal.classList.remove('active');
+}
 
 addBookButton.addEventListener("click", ()=>{
 
@@ -42,9 +74,14 @@ addBookButton.addEventListener("click", ()=>{
 
 
 
-submitButton.addEventListener("click", ()=>{
+bookForm.addEventListener("submit", ()=>{
 
+    event.preventDefault();
+    addNewBook();
+    removeModal();
     
 
 
 });
+
+// For Loop to display books
